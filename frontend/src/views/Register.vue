9fe42c4cea4a93810bef065b7ee952188aff123f
Gabriel Riboldi <g.riboldi@barbablanka.com>
2021-11-01 18:14:58 +0100

    [Front] Base Routing

---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="register">
+        <img id="chrono" alt="Vue logo" src="../assets/chrono.svg" />
+        <h1>Register</h1>
+
+        <input id="mb" v-model="username" placeholder="Choose a username" />
+        <input id="mb" v-model="password" placeholder="Choose a password" />
+
+        <button v-on:click="checkRegister()">Register</button>
+        <h3 style="color: red">{{ error && error }}</h3>
+    </div>
+</template>
+
+<script>
+import { getUser } from '../lib/user'
+// TODO: Ajouter le addUser to DB
+
+export default {
+    name: 'Register',
+    methods: {
+        async checkRegister() {
+            try {
+                const res = await getUser()
+
+                if (!res) {
+                    this.error = 'Username already used.'
+                }
+                console.log('check')
+                this.$router.push('/')
+            } catch (error) {
+                console.log('Error', error)
+            }
+        }
+    },
+    data() {
+        return {
+            username: '',
+            password: '',
+            error: ''
+        }
+    }
+}
+</script>
+
+<style>
+#register {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    top: 10%;
+}
+
+#chrono {
+    width: 30vh;
+}
+
+#mb {
+    margin-bottom: 10px;
+}
+</style>
