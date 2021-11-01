3a87e3f73bd37ac7792cf8a60c2e15dfa6523e24
Théo <zayyfix@gmail.com>
2021-11-01 17:49:15 +0100

    Add prettier config, add Navbar component

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="navbar">
+        <ul>
+            <li>
+                <router-link class="button" to="/"> Home </router-link>
+            </li>
+            <li>
+                <router-link class="button" to="/"> 1 </router-link>
+            </li>
+            <li>
+                <router-link class="button" to="/"> 2 </router-link>
+            </li>
+            <li>
+                <router-link class="button" to="/"> 3 </router-link>
+            </li>
+            <li>
+                <router-link class="button" to="/me"> Account </router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script></script>
+
+<style scoped>
+#navbar {
+    position: fixed;
+    width: 40%;
+    margin: auto;
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
+
+.button {
+    background-color: grey;
+    border-radius: 10px;
+    width: 5vw;
+    height: 5vw;
+    display: inline-block;
+    margin: 1%;
+}
+
+.button.router-link-active {
+    background-color: green;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+
+li {
+    display: inline;
+}
+
+img {
+    margin: auto;
+    transform: translateY(25%);
+}
+</style>
---
+++ frontend/src/views/User.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>User account view</h1>
+        <h1>(delete account, etc)</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String,
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
