12f98c4bad21ce10534e089bdf8c62149cb4c5a1
Alphamplyer <even@pouyadou.fr>
2021-11-01 19:18:44 +0100

    disable security for now

--- run_server.sh
+++ run_server.sh
@@ -7,5 +7,8 @@ mvn clean package -DskipTests
 cd ".."
 
 docker-compose up --build -d timemanager-postgresql
+
+sleep 10
+
 docker-compose up --build -d adminer
 docker-compose up --build -d timemanager-back
