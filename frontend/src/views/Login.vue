aecce3042891935262d1a1acf067cdefda91fc36
Gabriel Riboldi <g.riboldi@barbablanka.com>
2021-11-04 11:54:25 +0100

    [Front] LogIn/Register design

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,29 +1,69 @@
 <template>
-    <v-container fill-height class="d-flex align-center justify-center">
+    <v-container fill-height class="d-flex flex-row mr-12 align-center justify-center">
         <v-responsive 
-            style="border: 1px solid; border-color: #becdda; background-color: white"
-            class="rounded-lg pa-12 text-h3 blue--text text-center"
+            style="border: 1px solid; border-color: #A7A7A7; background-color: white; font-size: 42px"
+            class="rounded-lg pa-8 blue--text text-center"
             max-width="350px" 
             min-width="250px"
         >
-            <h3 class="mb-8">Time Manager</h3>
+            <h3 
+                style="line-height: 1"
+                class="mb-8"
+            >
+                Time Manager
+            </h3>
+
             <v-divider style="background-color: #becdda" />
-            <v-container class="my-4">
+
+            <v-container class="py-4">
                 <v-text-field 
                     solo
                     label="Username"
+                    class="text-center"
                 />
-                <v-text-field solo label="Password" />
+
+                <v-text-field 
+                    solo 
+                    label="Password" 
+                />
             </v-container>
+
             <v-divider style="background-color: #becdda" />
-            <v-btn 
-                block
-                color="primary"
-                class="mt-8"
-            >
-                Log In
-            </v-btn>
-            <h5 class="text-h6 thin">Register</h5>
+
+            <v-container class="d-flex flex-column justify-space-between">
+                <v-btn 
+                    block
+                    color="primary"
+                    style="font-size: 18px; border: 2px solid black !important"
+                    class="font-weight-bold rounded-0 text-capitalize"
+                    v-on:click="logIn()"
+                >
+                    Log In
+                </v-btn>
+
+                <h5 
+                    style="font-size: 14px"
+                    class="my-8 grey--text"
+                >
+                    Or
+                </h5>
+
+                <v-btn 
+                    style="font-size: 18px; border: 2px solid black"
+                    class="rounded-0 text-capitalize font-weight-bold"
+                    v-on:click="register()"
+                >
+                    Register
+                </v-btn>
+            </v-container>
+        </v-responsive>
+
+        <v-responsive>
+            <v-img
+                src="@/assets/welcome.svg"
+                max-height="700px"
+                lazy
+            />
         </v-responsive>
     </v-container>
 </template>
@@ -31,6 +71,14 @@
 <script>
 
 export default {
+    methods: {
+        register() {
+            this.$router.push('/register')
+        },
+        logIn() {
+            this.$router.push('/user/dashboard')
+        }
+    },
     setup() {
         
     },
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,10 +1,68 @@
 <template>
-    <div>Register</div>
+    <v-container fill-height class="d-flex flex-row mr-12 align-center justify-center">
+        <v-responsive 
+            style="border: 1px solid; border-color: #A7A7A7; background-color: white; font-size: 48px"
+            class="rounded-lg pa-8 blue--text text-center"
+            max-width="350px" 
+            min-width="250px"
+        >
+            <h3 class="mb-8">Register</h3>
+
+            <v-divider style="background-color: #becdda" />
+
+            <v-container class="py-4">
+                <v-text-field 
+                    solo
+                    label="Email"
+                />
+
+                <v-text-field 
+                    solo
+                    label="Username"
+                />
+
+                <v-text-field 
+                    solo 
+                    label="Password" 
+                />
+            </v-container>
+
+            <v-divider style="background-color: #becdda" />
+
+            <v-container class="d-flex flex-column justify-space-between">
+                <v-btn 
+                    block
+                    color="primary"
+                    style="font-size: 18px; border: 2px solid black !important"
+                    class="font-weight-bold rounded-0 text-capitalize"
+                    v-on:click="logIn()"
+                >
+                    Register
+                </v-btn>
+            </v-container>
+        </v-responsive>
+
+        <v-responsive>
+            <v-img
+                src="@/assets/register.svg"
+                max-height="700px"
+                lazy
+            />
+        </v-responsive>
+    </v-container>
 </template>
 
 <script>
-    export default {
-        name: 'Register',
 
-    }
+export default {
+    methods: {
+        logIn() {
+            this.$router.push('/')
+        }
+    },
+    setup() {
+        
+    },
+}
 </script>
+
