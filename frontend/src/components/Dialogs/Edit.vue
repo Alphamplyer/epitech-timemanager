83bdb083c9e1fffa6baa9dc4f314527ca99d60f1
Théo <zayyfix@gmail.com>
2021-11-08 11:08:45 +0100

    Edit user modal

---
+++ frontend/src/components/Dialogs/Edit.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-dialog v-model="dialogEdit" width="30%">
+      <template v-slot:activator="{ on, attrs }">
+        <div v-bind="attrs" v-on="on"
+          ><v-icon>mdi-pencil-outline</v-icon>
+          Edit user</div
+        >
+      </template>
+      <v-card>
+        <v-text-field
+          v-model="firstname"
+          :rules="nameRules"
+          :counter="10"
+          label="First name"
+          required
+        ></v-text-field>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialogEdit = false">YES</v-btn>
+          <v-btn color="error" text @click="dialogEdit = false">NO</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "Delete",
+  props: ["object", "type"],
+  data() {
+    return {
+      dialogEdit: false,
+    };
+  },
+};
+</script>
--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -9,7 +9,7 @@
                 <div class="secondRowLeft">
                     <v-icon class="userIcon">mdi-account-circle</v-icon>
                     <div class="changeUser">
-                        <h3><v-icon>mdi-pencil-outline</v-icon><span>Edit User</span></h3>
+                        <h3><span><Edit /></span></h3>
                         <h3><v-icon>mdi-account</v-icon><span>Promote User</span></h3>
                         <h3><v-icon>mdi-delete</v-icon><span>Delete User</span></h3>
                     </div>
@@ -35,11 +35,13 @@
 </template>
 
 <script>
+import Edit from '../components/Dialogs/Edit.vue'
 import Navbar from '../components/Navbar.vue'
 
 export default {
     components: {
-        Navbar
+        Navbar,
+        Edit
     },
     data() {
         return {
