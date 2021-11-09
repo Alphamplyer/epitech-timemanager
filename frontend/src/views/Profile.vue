1b09f8bb30d7880453296fa48f29a150d700972c
Théo <zayyfix@gmail.com>
2021-11-09 14:31:25 +0100

    🐛 Fix profile view (token -> vuex)

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -9,9 +9,9 @@
                 <div class="secondRowLeft">
                     <v-icon class="userIcon">mdi-account-circle</v-icon>
                     <div class="changeUser">
-                        <h3 style="display: flex"><v-icon>mdi-pencil-outline</v-icon><span><Edit :object="token.user"/></span></h3>
-                        <h3 style="display: flex"><v-icon>mdi-account</v-icon><span><Promote :object="token.user"/></span></h3>
-                        <h3 style="display: flex"><v-icon>mdi-delete</v-icon><span><Delete :object="token.user" :type="'userType'"/></span></h3>
+                        <h3 style="display: flex"><v-icon>mdi-pencil-outline</v-icon><span><Edit :object="account"/></span></h3>
+                        <h3 style="display: flex"><v-icon>mdi-account</v-icon><span><Promote :object="account"/></span></h3>
+                        <h3 style="display: flex"><v-icon>mdi-delete</v-icon><span><Delete :object="account" :type="'userType'"/></span></h3>
                     </div>
                 </div>
                 <div class="secondRowRight">
